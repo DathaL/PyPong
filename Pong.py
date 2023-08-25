9c20e06e4f5799ab1d0a56c3857c8c459c123560
DathaL <thanhdat.luong248@gmail.com>
2023-08-25 11:40:54 +0200

    Added OOP to the code.

--- Pong.py
+++ Pong.py
@@ -1,46 +1,50 @@
 import pygame
 from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((640,360))
-pygame.display.set_caption("Pong")
-clock = pygame.time.Clock()
-font = pygame.font.Font(None, 50) #insert retro font for score
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640,360))
+        pygame.display.set_caption("Pong")
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 50) #insert retro font for score
+        self.active = True
+        #Score
+        self.left_score = 0
+        self.right_score = 0
 
-game_active = True
-
-#Score
-left_score = 0
-right_score = 0
-
-#Images
-bg_surf = pygame.image.load("Pictures/background.png").convert()
-
-#Player
-player_surf = pygame.image.load("Pictures/Player.png").convert_alpha()
-left_player_rect = player_surf.get_rect(center = (30,180))
-right_player_rect = player_surf.get_rect(center= (610,180))
-
-#Ball
-ball_surf = pygame.image.load("Pictures/Ball.png")
-ball_rect = ball_surf.get_rect(center=(320,180))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    if game_active:
-        #Background
-        screen.blit(bg_surf,(0,0))
+        #Images
+        self.bg_surf = pygame.image.load("Pictures/background.png").convert()
 
         #Player
-        screen.blit(player_surf,left_player_rect)
-        screen.blit(player_surf,right_player_rect)
+        self.player_surf = pygame.image.load("Pictures/Player.png").convert_alpha()
+        self.left_player_rect = self.player_surf.get_rect(center = (30,180))
+        self.right_player_rect = self.player_surf.get_rect(center= (610,180))
 
         #Ball
-        screen.blit(ball_surf,ball_rect)
+        self.ball_surf = pygame.image.load("Pictures/Ball.png")
+        self.ball_rect = self.ball_surf.get_rect(center=(320,180))
 
-    pygame.display.update()
-    clock.tick(60)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            if self.active:
+                #Background
+                self.screen.blit(self.bg_surf,(0,0))
+
+                #Player
+                self.screen.blit(self.player_surf,self.left_player_rect)
+                self.screen.blit(self.player_surf,self.right_player_rect)
+
+                #Ball
+                self.screen.blit(self.ball_surf,self.ball_rect)
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
